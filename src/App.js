e3ba19bab8400ca95578b6edafcc52b810d70953
mceli004 <mceli004@umn.edu>
2023-01-29 13:48:39 -0600

    Bare bones point tracker added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 import Timer from './Components/Scoreboard/Timer.js'
+import PointTracker from './Components/Scoreboard/PointTracker';
 function App() {
+  const [winPoints, SetWinPoints] = useState(0);
+  const changeWinsHandler = (pointDelta) => {
+    SetWinPoints(prevPoints => {
+      return prevPoints += pointDelta;
+    });
+  }
   return (
     // <div className="App">
     //   <header className="App-header">
@@ -20,7 +28,10 @@ function App() {
     //   </header>
     // </div>
     <div className='App'>
-      <Timer/>
+      <div className='scoreboard__banner'>
+        <PointTracker value={winPoints} changePoint={changeWinsHandler} color='#0dc41cef'/>
+        <Timer/>
+      </div>
     </div>
   );
 }
---
+++ src/Components/Scoreboard/PointTracker.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './PointTracker.css'
+const PointTracker = (props) => {
+    const incrementPoint = () => {
+        props.changePoint(1);
+    }
+    return (
+        <div className='scoreboard__points'>
+            <h2 style={{'color':props.color}}>{props.value}</h2>
+            <button onClick={incrementPoint}>Click me</button>
+        </div>
+    )
+}
+
+export default PointTracker;
